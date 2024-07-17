6572653d77201b66f0fc5bc08b1c9f97651bc337
Chris Arsenault <chrisstanarsenault@gmail.com>
2024-07-17 09:42:53 -0400

    refactor: :recycle: code clean up

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,9 @@
 <template>
-  <!-- <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      :src="Logo"
-      width="125"
-      height="125"
-    >
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">
-          Home
-        </RouterLink>
-
-        <RouterLink to="/about">
-          About
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView /> -->
-
-  <Home />
+  <div class="mx-auto max-w-screen-layout">
+    <Home />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '#components/HelloWorld.vue'
-
 import Home from '#pages/root/home.vue'
-
-import Logo from '#assets/logo.svg'
 </script>
-
-<style scoped>
-
-</style>
