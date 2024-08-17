383df143f9c802896d80198db7fe8da080c2ed43
Chris Arsenault <chrisstanarsenault@gmail.com>
2024-08-17 13:20:50 -0400

    refactor: :recycle: changed vercel env name structure

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.firebase_apiKey,
-  authDomain: process.env.firebase_authDomain,
-  projectId: process.env.firebase_projectId,
-  storageBucket: process.env.firebase_storageBucket,
-  messagingSenderId: process.env.firebase_messagingSenderId,
-  appId: process.env.firebase_appId,
-  measurementId: process.env.firebase_measurementId,
+  apiKey: process.env.VITE_VERCEL_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_VERCEL_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_VERCEL_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_VERCEL_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
