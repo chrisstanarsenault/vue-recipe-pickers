1b9ca7b8c28dd7979418832eaba2b15638689867
Chris Arsenault <chrisstanarsenault@gmail.com>
2024-08-17 13:46:51 -0400

    refactor: :recycle: changed process.envs to import.meta

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.VITE_VERCEL_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
+  apiKey: import.meta.env.VITE_VERCEL_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_VERCEL_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_VERCEL_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_VERCEL_FIREBASE_APP_ID,
-  measurementId: process.env.VITE_VERCEL_FIREBASE_MEASUREMENT_ID,
+  storageBucket: import.meta.env.VITE_VERCEL_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_VERCEL_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_VERCEL_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_VERCEL_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
